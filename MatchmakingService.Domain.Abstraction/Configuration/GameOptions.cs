c1501d52281bd1efb78c605de90bc146c5c85fb5
Aliaksei Smaliuk <aliaksei.smaliuk@gmail.com>
2022-09-25 22:34:12 +0300

    Adjust game configuration repository.

--- MatchmakingService.Domain.Abstraction/Configuration/GameOptions.cs
+++ MatchmakingService.Domain.Abstraction/Configuration/GameOptions.cs
@@ -1,6 +1,10 @@
+using MatchmakingService.Domain.Abstraction.Models;
+
 namespace MatchmakingService.Domain.Abstraction.Configuration;
 
 public record GameOptions
 {
     public int MaxCashAmount { get; set; }
+    public GameConfiguration SmallGameConfiguration { get; set; } = null!;
+    public GameConfiguration StandardGameConfiguration { get; set; } = null!;
 }
